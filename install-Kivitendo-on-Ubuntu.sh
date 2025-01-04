8517142c8d0b1da92cf0f251771ffef64d05c6a6
Ronny Zimmermann <ronny@inter-data.de>
2025-01-04 21:09:53 +0100

    Reine Installation von Kivitendo ohne CRM.
    Das CRM-Projekt wird mit Vue neu geschrieben.

--- install-Kivitendo-on-Ubuntu.sh
+++ install-Kivitendo-on-Ubuntu.sh
@@ -1,61 +1,76 @@
 #!/bin/bash
-set +e
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
+# Aktualisierung der Paketquellen
+echo "Aktualisiere die Paketquellen..."
+sudo apt update
 
-## Memo und Script zur Installation von kivitendo unter Ubuntu 18.04 Bionic (LTS)
-echo "Pakete installieren"
-apt-get update && apt-get upgrade
+# Installation der benötigten Pakete
+echo "Installiere erforderliche Pakete..."
+sudo apt install -y apache2 libarchive-zip-perl libclone-perl \
+  libconfig-std-perl libdatetime-perl libdbd-pg-perl libdbi-perl \
+  libemail-address-perl libemail-mime-perl libfcgi-perl libjson-perl \
+  liblist-moreutils-perl libnet-smtp-ssl-perl libnet-sslglue-perl \
+  libparams-validate-perl libpdf-api2-perl librose-db-object-perl \
+  librose-db-perl librose-object-perl libsort-naturally-perl \
+  libstring-shellquote-perl libtemplate-perl libtext-csv-xs-perl \
+  libtext-iconv-perl liburi-perl libxml-writer-perl libyaml-perl \
+  libimage-info-perl libgd-gd2-perl libapache2-mod-fcgid \
+  libfile-copy-recursive-perl postgresql libalgorithm-checkdigits-perl \
+  libcrypt-pbkdf2-perl git libcgi-pm-perl libtext-unidecode-perl libwww-perl \
+  postgresql-contrib poppler-utils libhtml-restrict-perl \
+  libdatetime-set-perl libset-infinite-perl liblist-utilsby-perl \
+  libdaemon-generic-perl libfile-flock-perl libfile-slurp-perl \
+  libfile-mimeinfo-perl libpbkdf2-tiny-perl libregexp-ipv6-perl \
+  libdatetime-event-cron-perl libexception-class-perl \
+  libxml-libxml-perl libtry-tiny-perl libmath-round-perl \
+  libimager-perl libimager-qrcode-perl librest-client-perl libipc-run-perl \
+  libencode-imaputf7-perl libmail-imapclient-perl libuuid-tiny-perl dialog
 
-apt-get install make gcc apache2 libapache2-mod-fcgid libarchive-zip-perl libclone-perl libconfig-std-perl libdatetime-perl libdbd-pg-perl libdbi-perl libemail-address-perl libemail-mime-perl libfcgi-perl libjson-perl liblist-moreutils-perl libnet-smtp-ssl-perl libnet-sslglue-perl libparams-validate-perl libpdf-api2-perl librose-db-object-perl librose-db-perl libipc-run-perl librose-object-perl libsort-naturally-perl libstring-shellquote-perl libtemplate-perl libtext-csv-xs-perl libtext-iconv-perl liburi-perl libxml-writer-perl libyaml-perl libfile-copy-recursive-perl libgd-gd2-perl libimage-info-perl libalgorithm-checkdigits-perl postgresql git perl-doc libapache2-mod-php php-ssh2 php-gd php-imap php-mail php-mail-mime php-pgsql php-fpdf imagemagick fonts-freefont-ttf php-curl dialog php-enchant aspell-de libcgi-pm-perl libdatetime-set-perl libfile-mimeinfo-perl liblist-utilsby-perl libpbkdf2-tiny-perl libregexp-ipv6-perl libtext-unidecode-perl libdaemon-generic-perl libfile-flock-perl libfile-slurp-perl libset-crontab-perl libdatetime-event-cron-perl python3 python3-serial libcam-pdf-perl libmath-round-perl libhtml-restrict-perl libimager-perl libimager-qrcode-perl librest-client-perl ttf-mscorefonts-installer
-
-a2enmod cgi
-
-pear install  Contact_Vcard_Build Contact_Vcard_Parse
-
-
+# Latex-Installation
 dialog --title "Latex installieren" --backtitle "kivitendo installieren" --yesno "Möchten Sie Latex installieren?" 7 60
-
-
 response=$?
 case $response in
    0) echo "Latex wird installiert."
-      apt-get install texlive-binaries texlive-latex-recommended texlive-fonts-recommended texlive-lang-german dvisvgm fonts-lmodern fonts-texgyre libptexenc1 libsynctex2 libteckit0 libtexlua53 libtexluajit2 libzzip-0-13 lmodern tex-common tex-gyre texlive-base latexmk texlive-latex-extra
-
+      apt-get install -y texlive-binaries texlive-latex-recommended texlive-fonts-recommended \
+      texlive-lang-german dvisvgm fonts-lmodern fonts-texgyre libptexenc1 libsynctex2 \
+      libteckit0 libtexlua53 libtexluajit2 libzzip-0-13 lmodern tex-common tex-gyre \
+      texlive-base latexmk texlive-latex-extra
       ;;
    1) echo "Latex wird nicht installiert."
       ;;
 esac
 
-##Dialog Passwd
-dialog --clear --title "Dialog Password" --backtitle "kivitendo installieren" --inputbox "Achtung, Password in Beispieldatenbank bleibt unverändert. (kivitendo)" 10 50 2>/tmp/kivitendo_passwd.$$ kivitendo
-PASSWD=`cat /tmp/kivitendo_passwd.$$`
+# Passwort eingeben
+dialog --clear --title "Dialog Password" --backtitle "kivitendo installieren" \
+--inputbox "Wähle ein Passwd für kivitendo und postgres" 10 50 "kivitendo" 2>/tmp/kivitendo_passwd.$$
+PASSWD=$(cat /tmp/kivitendo_passwd.$$)
+rm -f /tmp/kivitendo_passwd.$$
 
-##Dialog Directory
-DIR=/var/www
-dialog --clear --title "Dialog Installationsverzeichnis" --backtitle "kivitendo installieren" --inputbox "Pfad ohne abschließenden Slash eingenben" 10 50 2>/tmp/kivitendo_dir.$$ /var/www
-DIR=`cat /tmp/kivitendo_dir.$$`
-rm -f /tmp/kivitendo*
+# Installationsverzeichnis eingeben
+dialog --clear --title "Dialog Installationsverzeichnis" --backtitle "kivitendo installieren" \
+--inputbox "Pfad ohne abschließenden Slash eingeben" 10 50 "/var/www" 2>/tmp/kivitendo_dir.$$
+DIR=$(cat /tmp/kivitendo_dir.$$)
+rm -f /tmp/kivitendo_dir.$$
 
-
-cd $DIR
+# Klonen des kivitendo-ERP-Repositories
+echo "Klone das kivitendo-ERP-Repository..."
+cd /var/www/
 git clone https://github.com/kivitendo/kivitendo-erp.git
-git clone https://github.com/kivitendo/kivitendo-crm.git
 
-echo "ERP-Plugins installieren"
-sed -i '$adocument.write("<script type='text/javascript' src='crm/js/ERPplugins.js'></script>")' kivitendo-erp/js/kivi.js
+# Wechsel in das kivitendo-Verzeichnis und Checkout des neuesten stabilen Tags
+echo "Wechsle zum neuesten stabilen Release von kivitendo..."
+cd kivitendo-erp/
+git checkout $(git tag -l | egrep -ve "(alpha|beta|rc)" | tail -1)
+chown -R www-data: "$DIR/kivitendo-erp"
 
-
+# Virtuellen Host anlegen
 echo "Virtuellen Host anlegen"
 if [ -f /etc/apache2/sites-available/kivitendo.apache2.conf ]; then
     echo "Lösche vorherigen Virtuellen Host"
     rm -f /etc/apache2/sites-available/kivitendo.apache2.conf
 fi
-touch /etc/apache2/sites-available/kivitendo.apache2.conf
-echo "AddHandler fcgid-script .fpl
+cat <<EOL > /etc/apache2/sites-available/kivitendo.apache2.conf
+AddHandler fcgid-script .fpl
 AliasMatch ^/kivitendo/[^/]+\.pl $DIR/kivitendo-erp/dispatcher.fcgi
 Alias       /kivitendo/          $DIR/kivitendo-erp/
 <Directory $DIR/kivitendo-erp>
@@ -69,81 +84,20 @@ Alias       /kivitendo/          $DIR/kivitendo-erp/
 <Directory $DIR/kivitendo-erp/users>
   Require all denied
 </Directory>
-<Directory $DIR/kivitendo-crm>
-  AddDefaultCharset UTF-8
-  Require all denied
-</Directory>
-" >>  /etc/apache2/sites-available/kivitendo.apache2.conf
+EOL
 ln -sf /etc/apache2/sites-available/kivitendo.apache2.conf /etc/apache2/sites-enabled/kivitendo.apache2.conf
 service apache2 restart
 
+# Postgres-Passwort ändern
 echo "postgres Password ändern"
 sudo -u postgres -H -- psql -d template1 -c "ALTER ROLE postgres WITH password '$PASSWD'"
 
+# Konfigurationsdatei erzeugen und anpassen
 echo "config/kivitendo.conf erzeugen"
 cp -f $DIR/kivitendo-erp/config/kivitendo.conf.default $DIR/kivitendo-erp/config/kivitendo.conf
-
-echo "kivitendo.conf bearbeiten"
 sed -i "s/admin_password.*$/admin_password = $PASSWD/" $DIR/kivitendo-erp/config/kivitendo.conf
 sed -i "s/password =$/password = $PASSWD/" $DIR/kivitendo-erp/config/kivitendo.conf
 
-
-chown -R www-data: *
-cd $DIR/kivitendo-erp/
-ln -s ../kivitendo-crm/ crm
-
-##Menü verlinken oder kopieren:
-cd $DIR/kivitendo-erp/menus/user
-ln -s ../../../kivitendo-crm/menu/10-crm-menu.yaml 10-crm-menu.yaml
-
-##Rechte für CRM ermöglichen:
-cd $DIR/kivitendo-erp/sql/Pg-upgrade2-auth
-ln -s  ../../../kivitendo-crm/update/add_crm_master_rights.sql add_crm_master_rights.sql
-
-##Übersetzungen anlegen:
-cd $DIR/kivitendo-erp/locale/de
-mkdir more
-ln -s ../../../../kivitendo-crm/menu/t8e/menu.de crm-menu.de
-ln -s ../../../../kivitendo-crm/menu/t8e/menu-admin.de crm-menu-admin.de
-
-var=$(git tag | xargs -I@ git log --format=format:"%ai @%n" -1 @ | sort | awk '{print $4,v++,"off"}' | tail -n 8)
-_temp="/tmp/answer.$$"
-
-dialog --backtitle "ERP-Version wählen, ESC für Git" --radiolist "Wähle Tag der ausgecheckt werden soll, ESC für aktuelle Git-Version!" 20 50 8 $var 2>$_temp
-result=`cat $_temp`
-
-gitlog=$(git log -1 --pretty=oneline --abbrev-commit)
-
-if [ -z "$result" ]; then
-     dialog --title "Aktuelle Git" --msgbox "Aktuelle Entwicklerversion:\n$gitlog" 8 66
-else
-    dialog --title "Ausgewählter Tag" --msgbox "$result wird ausgecheckt!" 6 44
-    git checkout $result
-fi
-
-
-dialog --title "Datenbank installieren" --backtitle "kivitendo installieren" --yesno "Möchten Sie die Beispiel-Datenbank für Version 3.2.x installieren?" 7 60
-response=$?
-case $response in
-    0) echo "Datenbank wird installiert."
-    sudo -u postgres -H -- createdb kivitendo_auth
-    sudo -u postgres -H -- createdb demo-db
-    sudo -u postgres -H -- psql kivitendo_auth < $DIR/kivitendo-crm/install/kivitendo_auth.sql
-    sudo -u postgres -H -- psql demo-db < $DIR/kivitendo-crm/install/demo-db.sql
-    echo "Beim Login: Benutzername: demo, Password: kivitendo"
-    echo "***************************************************"
-    if [ "$PASSWD" != "kivitendo" ]; then
-        echo "Es wurde ein eigenes Passwort vergeben."
-        echo "Dieses Passwort muss in der Mandantenkonfiguration eingetragen werden!"
-        echo "(http://localhost/kivitendo/admin.pl)"
-    fi
-    ;;
-    1) echo "Datenbank wird nicht installiert."
-       ;;
-esac
-
-
-
-echo "......Installation beendet"
-echo ""
-echo "kivitendo kann jetzt im Browser unter http://localhost/kivitendo/ aufgerufen werden"
+# Abschlussmeldungen
+echo "kivitendo kann jetzt im Browser unter http://localhost/kivitendo/ aufgerufen werden!"
+echo "Im Adminbereich können Sie Datenbanken, Benutzer und Gruppen hinzufügen sowie weitere Konfigurationen vornehmen."
